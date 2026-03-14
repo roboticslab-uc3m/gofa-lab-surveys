4fd23387329793b1a7b3a42b13156387f4fed899
Bartek Łukawski <bwmn.peter@gmail.com>
2026-03-14 15:16:39 +0100

    Make the default linter happy

--- survey-sime-2026.r
+++ survey-sime-2026.r
@@ -1,83 +1,97 @@
 library(HH)
 library(readODS)
 
-survey <- read_ods("D:/survey-sime-2026.ods",
-                   sheet="sanitized", range="A1:H60")
+survey <- read_ods(
+  "D:/survey-sime-2026.ods",
+  sheet = "sanitized",
+  range = "A1:H60"
+)
 
-# View(survey)
+printf_pdf <- function(plot, filename, width, height, custom_fn = NULL) {
+  pdf(filename, width = width, height = height)
+  print(plot)
 
-printf_pdf <- function(plot, filename, width, height, custom_fn=NULL) {
-    pdf(filename, width=width, height=height)
-    print(plot)
+  if (!is.null(custom_fn)) {
+    custom_fn()
+  }
 
-    if (!is.null(custom_fn)) {
-        custom_fn()
-    }
-
-    dev.off()
+  dev.off()
 }
 
-do_column_plots <- function(question, title, reduced=FALSE) {
-    FilteredRows <- survey$Question == question
+do_column_plots <- function(question, title, reduced = FALSE) {
+  filtered_rows <- survey$Question == question
 
-    PercentPlot <-
-        likert(Group ~ . | Subtable, data=survey[FilteredRows,],
-               as.percent=TRUE,
-               ylab=NULL,
-               main=NULL,
-               strip=FALSE,
-               par.strip.text=list(cex=.6, lines=5),
-               layout=c(1,2),
-               scales=list(y=list(relation="free")),
-               auto.key=FALSE,
-               aspect=if (reduced) 0.1 else "fill")
+  percent_plot <- likert(
+    Group ~ . | Subtable,
+    data = survey[filtered_rows, ],
+    as.percent = TRUE,
+    ylab = NULL,
+    main = NULL,
+    strip = FALSE,
+    par.strip.text = list(cex = 0.6, lines = 5),
+    layout = c(1, 2),
+    scales = list(y = list(relation = "free")),
+    auto.key = FALSE,
+    aspect = if (reduced) 0.1 else "fill"
+  )
 
-    CountPlot <-
-        likert(Group ~ . | Subtable, data=survey[FilteredRows,],
-               ylab=NULL,
-               rightAxis=TRUE,
-               main=NULL,
-               strip=FALSE,
-               par.strip.text=list(cex=.6, lines=5),
-               layout=c(1,2),
-               scales=list(y=list(relation="free")),
-               auto.key=FALSE,
-               aspect=if (reduced) 0.27 else "fill")
+  count_plot <- likert(
+    Group ~ . | Subtable,
+    data = survey[filtered_rows, ],
+    ylab = NULL,
+    rightAxis = TRUE,
+    main = NULL,
+    strip = FALSE,
+    par.strip.text = list(cex = 0.6, lines = 5),
+    layout = c(1, 2),
+    scales = list(y = list(relation = "free")),
+    auto.key = FALSE,
+    aspect = if (reduced) 0.27 else "fill"
+  )
 
-    PercentCountPlot <-
-        as.TwoTrellisColumns5(update(PercentPlot,
-                                     rightAxis=FALSE),
-                              update(CountPlot,
-                                     ylab.right="Totals"),
-                              pw=c(.09, .59, .01, .22, .09))
+  percent_count_plot <- as.TwoTrellisColumns5(
+    update(percent_plot, rightAxis = FALSE),
+    update(count_plot, ylab.right = "Totals"),
+    pw = c(0.09, 0.59, 0.01, 0.22, 0.09)
+  )
 
-    printf_pdf(PercentCountPlot, sprintf("%s.pdf", question),
-               width=9,
-               height=if (reduced) 3 else 4.5,
-               custom_fn=function() {
-                   grid.text(title,
-                             y=unit(0.97, "npc"),
-                             gp=gpar(fontsize=14))
-                   })
+  printf_pdf(
+    percent_count_plot,
+    sprintf("%s.pdf", question),
+    width = 9,
+    height = if (reduced) 3 else 4.5,
+    custom_fn = function() {
+      grid::grid.text(
+        title,
+        y = grid::unit(0.97, "npc"),
+        gp = grid::gpar(fontsize = 14)
+      )
+    }
+  )
 }
 
 do_single_plot <- function(question, title) {
-    PercentPlot <-
-        likert(Group ~ . | Subtable, data=survey[survey$Question == question,],
-               as.percent=TRUE,
-               ylab=NULL,
-               ylab.right="Totals",
-               main=list(title, fontface=1),
-               strip=FALSE,
-               par.strip.text=list(cex=.6, lines=5),
-               layout=c(1,1),
-               scales=list(y=list(relation="free")),
-               auto.key=FALSE,
-               aspect=0.1)
+  percent_plot <- likert(
+    Group ~ . | Subtable,
+    data = survey[survey$Question == question, ],
+    as.percent = TRUE,
+    ylab = NULL,
+    ylab.right = "Totals",
+    main = list(title, fontface = 1),
+    strip = FALSE,
+    par.strip.text = list(cex = 0.6, lines = 5),
+    layout = c(1, 1),
+    scales = list(y = list(relation = "free")),
+    auto.key = FALSE,
+    aspect = 0.1
+  )
 
-    printf_pdf(PercentPlot, sprintf("%s.pdf", question),
-               width=9,
-               height=2.4)
+  printf_pdf(
+    percent_plot,
+    sprintf("%s.pdf", question),
+    width = 9,
+    height = 2.4
+  )
 }
 
 do_column_plots("drawing-1", "I found the drawing task entertaining")
@@ -87,8 +101,8 @@ do_column_plots("drawing-4", "Code transfer between simulation (RobotStudio) and
 do_column_plots("drawing-5", "I had no issues while running the simulation in RobotStudio")
 do_column_plots("drawing-6", "I had no issues while running the same code on the real robot after transferring it from RobotStudio")
 
-do_column_plots("magnet-1", "I found the pick-and-place task entertaining", reduced=TRUE)
-do_column_plots("magnet-2", "I managed to successfully complete the pick-and-place task", reduced=TRUE)
+do_column_plots("magnet-1", "I found the pick-and-place task entertaining", reduced = TRUE)
+do_column_plots("magnet-2", "I managed to successfully complete the pick-and-place task", reduced = TRUE)
 do_single_plot("magnet-3", "Our group managed to complete the pick-and-place task with little or no assistance from the instructor")
 do_single_plot("magnet-4", "Having a code template helped me complete the pick-and-place task")
 
--- survey-teaching-innovation-2026.r
+++ survey-teaching-innovation-2026.r
@@ -1,83 +1,96 @@
 library(HH)
 library(readODS)
 
-survey <- read_ods("D:/survey-teaching-innovation-2026.ods",
-                   sheet="sanitized", range="A1:H70")
+survey <- read_ods(
+  "D:/survey-teaching-innovation-2026.ods",
+  sheet = "sanitized",
+  range = "A1:H70"
+)
 
-# View(survey)
+printf_pdf <- function(plot, filename, width, height, custom_fn = NULL) {
+  pdf(filename, width = width, height = height)
+  print(plot)
 
-printf_pdf <- function(plot, filename, width, height, custom_fn=NULL) {
-    pdf(filename, width=width, height=height)
-    print(plot)
+  if (!is.null(custom_fn)) {
+    custom_fn()
+  }
 
-    if (!is.null(custom_fn)) {
-        custom_fn()
-    }
-
-    dev.off()
+  dev.off()
 }
 
-do_column_plots <- function(question, title, reduced=FALSE) {
-    FilteredRows <- survey$Question == question
+do_column_plots <- function(question, title, reduced = FALSE) {
+  filtered_rows <- survey$Question == question
 
-    PercentPlot <-
-        likert(Group ~ . | Subtable, data=survey[FilteredRows,],
-               as.percent=TRUE,
-               ylab=NULL,
-               main=NULL,
-               strip=FALSE,
-               par.strip.text=list(cex=.6, lines=5),
-               layout=c(1,2),
-               scales=list(y=list(relation="free")),
-               auto.key=FALSE,
-               aspect=if (reduced) 0.1 else "fill")
+  percent_plot <-
+    likert(
+      Group ~ . | Subtable, data = survey[filtered_rows, ],
+      as.percent = TRUE,
+      ylab = NULL,
+      main = NULL,
+      strip = FALSE,
+      par.strip.text = list(cex = .6, lines = 5),
+      layout = c(1, 2),
+      scales = list(y = list(relation = "free")),
+      auto.key = FALSE,
+      aspect = if (reduced) 0.1 else "fill"
+    )
 
-    CountPlot <-
-        likert(Group ~ . | Subtable, data=survey[FilteredRows,],
-               ylab=NULL,
-               rightAxis=TRUE,
-               main=NULL,
-               strip=FALSE,
-               par.strip.text=list(cex=.6, lines=5),
-               layout=c(1,2),
-               scales=list(y=list(relation="free")),
-               auto.key=FALSE,
-               aspect=if (reduced) 0.27 else "fill")
+  count_plot <-
+    likert(
+      Group ~ . | Subtable, data = survey[filtered_rows, ],
+      ylab = NULL,
+      rightAxis = TRUE,
+      main = NULL,
+      strip = FALSE,
+      par.strip.text = list(cex = .6, lines = 5),
+      layout = c(1, 2),
+      scales = list(y = list(relation = "free")),
+      auto.key = FALSE,
+      aspect = if (reduced) 0.27 else "fill"
+    )
 
-    PercentCountPlot <-
-        as.TwoTrellisColumns5(update(PercentPlot,
-                                     rightAxis=FALSE),
-                              update(CountPlot,
-                                     ylab.right="Totals"),
-                              pw=c(.09, .59, .01, .22, .09))
+  percent_count_plot <-
+    as.TwoTrellisColumns5(
+      update(percent_plot, rightAxis = FALSE),
+      update(count_plot, ylab.right = "Totals"),
+      pw = c(.09, .59, .01, .22, .09)
+    )
 
-    printf_pdf(PercentCountPlot, sprintf("%s.pdf", question),
-               width=9,
-               height=if (reduced) 3 else 4.5,
-               custom_fn=function() {
-                   grid.text(title,
-                             y=unit(0.97, "npc"),
-                             gp=gpar(fontsize=14))
-                   })
+  printf_pdf(
+    percent_count_plot, sprintf("%s.pdf", question),
+    width = 9,
+    height = if (reduced) 3 else 4.5,
+    custom_fn = function() {
+      grid::grid.text(
+        title,
+        y = grid::unit(0.97, "npc"),
+        gp = grid::gpar(fontsize = 14)
+      )
+    }
+  )
 }
 
 do_single_plot <- function(question, title) {
-    PercentPlot <-
-        likert(Group ~ . | Subtable, data=survey[survey$Question == question,],
-               as.percent=TRUE,
-               ylab=NULL,
-               ylab.right="Totals",
-               main=list(title, fontface=1),
-               strip=FALSE,
-               par.strip.text=list(cex=.6, lines=5),
-               layout=c(1,1),
-               scales=list(y=list(relation="free")),
-               auto.key=FALSE,
-               aspect=0.1)
+  percent_plot <-
+    likert(
+      Group ~ . | Subtable, data = survey[survey$Question == question, ],
+      as.percent = TRUE,
+      ylab = NULL,
+      ylab.right = "Totals",
+      main = list(title, fontface = 1),
+      strip = FALSE,
+      par.strip.text = list(cex = .6, lines = 5),
+      layout = c(1, 1),
+      scales = list(y = list(relation = "free")),
+      auto.key = FALSE,
+      aspect = 0.1
+    )
 
-    printf_pdf(PercentPlot, sprintf("%s.pdf", question),
-               width=9,
-               height=2.4)
+  printf_pdf(
+    percent_plot, sprintf("%s.pdf", question),
+    width = 9,
+    height = 2.4
+  )
 }
 
 do_column_plots("drawing-1", "La tarea de dibujar me ha parecido entretenida")
@@ -87,8 +100,8 @@ do_column_plots("drawing-4", "La sincronización de código entre la simulación
 do_column_plots("drawing-5", "No he tenido problemas durante la simulación en RobotStudio")
 do_column_plots("drawing-6", "No he tenido problemas durante la ejecución del código en el robot real después de transferirlo desde RobotStudio")
 
-do_column_plots("magnet-1", "La tarea de pick-and-place me ha parecido entretenida", reduced=TRUE)
-do_column_plots("magnet-2", "He conseguido completar la tarea de pick-and-place exitosamente", reduced=TRUE)
+do_column_plots("magnet-1", "La tarea de pick-and-place me ha parecido entretenida", reduced = TRUE)
+do_column_plots("magnet-2", "He conseguido completar la tarea de pick-and-place exitosamente", reduced = TRUE)
 do_single_plot("magnet-3", "Mi grupo ha sido capaz de realizar la tarea de pick-and-place de forma autónoma")
 do_single_plot("magnet-4", "Disponer de una plantilla de código me ha ayudado a completar la tarea de pick-and-place")
 
